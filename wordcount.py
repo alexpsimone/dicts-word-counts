72c5ef34dceb4313a6d4ee4825ef83a2abb7b252
alexpsimone <alex.p.simone@gmail.com>
2020-10-05 19:26:14 +0000

    Tokenize input file

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,12 @@
-# put your code here.
+def count_words(filename):
+
+    lines = open(filename)
+    count_words = {}
+
+    for line in lines:
+        line = line.rstrip()
+        line = line.split(" ")
+        #line = [(words in that line, tokenized and put into this list)]
+        # for word in lines
+        print(line)
+    
